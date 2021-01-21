784f47ead39e164bbe88d0bc22363c38e0a6700a
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2021-01-21 14:05:49 +0100

    exemple ts typage

---
+++ exemples/ts-typage/index.ts
@@ -0,0 +1,17 @@
+const uneChaine: string = 'hello'
+
+let unNombre: number
+unNombre = 10
+
+const unTableau: number[] = [1, 2, 3]
+
+let uneVariable: any = 4
+uneVariable = "bonjour"
+
+let uneAutreVariable = "salut"
+
+function uneFonction(param1: string, param2: number): string {
+  return param1 + param2
+}
+
+console.log(uneFonction(uneChaine, unNombre))
